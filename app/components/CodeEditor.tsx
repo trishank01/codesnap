f401abd2a49729f92456008f8499e9c2f4f8e27a
tk <trishank01@gmail.com>
2025-06-04 23:33:27 +0530

    design and resize login for codeEditor

--- app/components/CodeEditor.tsx
+++ app/components/CodeEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Resizable } from "re-resizable";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AceEditor from "react-ace";
 
 // languages
@@ -38,9 +38,56 @@ const CodeEditor = ({
   background,
   currentPadding,
 }: CodeEditorProps) => {
+  const [width, setWidth] = useState(1000);
+  const [height, setHeight] = useState<number | null>(500);
+
+  // @ts-ignore
+  const handleResize = (event, direction, ref, pos) => {
+    const newHeight = ref.style.height;
+    setHeight(parseInt(newHeight));
+  };
+
+  const updateSize = () => {
+    setHeight(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateSize);
+    updateSize();
+
+    return () => {
+      window.removeEventListener("resize", updateSize);
+    };
+  }, []);
   return (
-    <Resizable minHeight={466} minWidth={510} maxWidth={1000}>
-      <div>
+    <Resizable
+      minHeight={466}
+      minWidth={510}
+      maxWidth={1000}
+      defaultSize={{
+        width: width,
+        height: height || 500,
+      }}
+      onResize={handleResize}
+      className="resize-container relative"
+    >
+      <div className="code-block">
+        <div className="code-title h-[52px] px-4 flex items-center justify-between bg-black opacity-80">
+          <div className="dots flex items-center gap-1">
+            <div className="w-3 h-3 rounded-full bg-[#ff5656]"></div>
+            <div className="w-3 h-3 rounded-full bg-[#ffbc6a]"></div>
+            <div className="w-3 h-3 rounded-full bg-[#67f772]"></div>
+          </div>
+          <div className="input-control w-full">
+            <input
+              type="text"
+              className="w-full text-[hsla(0,0%100%,.6)] outline-none font-medium text-center bg-transparent"
+            />
+          </div>
+          <div className="icon flex justify-center items-center p-1 bg-black bg-opacity-30 rounded-sm">
+            <img src={icon} alt="" />
+          </div>
+        </div>
         <AceEditor
           value="function(){return 'hello word';}"
           name="UNIQUE_ID_OF_DIV"
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-"use client"
+"use client";
 import { useState } from "react";
-import {languages} from "@/app/utils/utilities"
+import { languages } from "@/app/utils/utilities";
 import CodeEditor from "./components/CodeEditor";
 import LanguageSelector from "./components/LanguageSelector";
 
 export default function Home() {
-  const [language , setLanguage] = useState<string>(languages[0].name)
+  const [language, setLanguage] = useState(languages[0].name);
   return (
-    <main className="">
-      <header>
-        <LanguageSelector/>
+    <main className="h-screen flex flex-col items-center justify-between">
+      <header className="mt-6 w-[940px] p-5 fixed top-0 left-1/2 translate-x-[-50%] z-10 bg-[#191919] rounded-lg border border-[#3c3c3c shadow-md]">
+        <LanguageSelector />
       </header>
-      <div className="code-editor-ref">
-        <CodeEditor  language={language}/>
+      <div className="code-editor-ref mt-[10rem]">
+        <CodeEditor language={language} />
       </div>
     </main>
   );
